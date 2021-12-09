f1c6314d0a809ff126a9d450428d4640f084220d
Keefer Taylor <keefer.t@gmail.com>
2021-12-09 10:09:20 -0800

    Add function to calculate balances

--- src/liquidity-pool-client.ts
+++ src/liquidity-pool-client.ts
@@ -48,4 +48,40 @@ export default class LiquidityPoolClient {
     const sendArgs = { amount: 0, mutez: true }
     return await liquidityPoolContract.methods['liquidate'](targetOvenAddress).send(sendArgs)
   }
+  
+  /**
+   * Get the exchange rate of 1 LP token to kUSD as a string.
+   *
+   * @param precision The number of decimals to included
+   */ 
+  public async getExchangeRate(precision: number): Promise<string> {
+    // Load kUSD Contract
+    const kUSDContract = await this.tezos.wallet.at(contracts.TOKEN)
+    const kUSDStorage = await kUSDContract.storage()
+
+    // Load Liq Pool Contract
+    const liqContract = await this.tezos.wallet.at(contracts.LIQUIDITY_POOL)
+    const liqStorage = await liqContract.storage()
+
+    // Get number of kUSD in the liquidity pool
+    const poolBalance = await kUSDStorage.balances.get(contracts.LIQUIDITY_POOL)
+    if (poolBalance === undefined){
+      this.poolBalance = new BigNumber(0)
+    } else {
+      this.poolBalance = poolBalance.balance
+    } 
+
+    // Get number of LP tokens outstanding
+    const lpBalance = liqStorage.totalSupply
+
+    // Changed fixed point numbers into decimals 
+    const KUSD_MANTISSA = Math.pow(10, 18) // kUSD has 18 decimals
+    const LP_MANTISSA = Math.pow(10, 36).  // LP has 36 decimals
+    const kUSDBalanceDecimal = poolBalance.dividedBy(KUSD_MANTISSA)
+    const lpBalanceDecimal = lpBalance.dividedBy(LP_MATISSA)
+
+    // Calculate an exchange rate
+    // toFixed() will give you n digits of precision
+    const redeemRate = poolBalanceDecimal.dividedBy(lpBalanceDecimal).toFixed(2)
+  }
 }
