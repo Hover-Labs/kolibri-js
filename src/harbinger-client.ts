c4c2d0166b483eeef483a151c56576ef5e8fd77f
Alex Skene <510285+skenaja@users.noreply.github.com>
2024-12-06 12:50:13 +0000

    retrieve oracle data from youves contract
    replaces coinbase oracle which has been deprecated

--- src/harbinger-client.ts
+++ src/harbinger-client.ts
@@ -3,7 +3,7 @@ import { TezosToolkit } from '@taquito/taquito'
 import Mutez from './types/mutez'
 
 /** Asset pair to query */
-const ASSET_CODE = 'XTZ-USD'
+const ASSET_CODE = 'XTZUSDT'
 
 /**
  * Price feed data.
@@ -24,9 +24,14 @@ export default class HarbingerClient {
    * Construct a new Oracle Client.
    *
    * @param nodeUrl The URL of the node to connect to.
-   * @param normalizerAddress The address of the Normalizer contract to read from.
+   * @param oracleAddress The address of the Youves Oracle contract to read price from.
+   * @param ovenProxyAddress The address of the oven proxy that is calling for the oracle data.
    */
-  public constructor(nodeUrl: string, public readonly normalizerAddress: Address) {
+  public constructor(
+    nodeUrl: string,
+    public readonly oracleAddress: Address,
+    public readonly ovenProxyAddress: Address,
+  ) {
     this.tezos = new TezosToolkit(nodeUrl)
   }
 
@@ -34,13 +39,15 @@ export default class HarbingerClient {
    * Retrieve price feed data.
    */
   public async getPriceData(): Promise<HarbingerPriceFeedData> {
-    const normalizerContract = await this.tezos.contract.at(this.normalizerAddress)
-    const normalizerStorage: any = await normalizerContract.storage()
-    const assetData = await normalizerStorage.assetMap.get(ASSET_CODE)
+    const oracleContract = await this.tezos.contract.at(this.oracleAddress)
+    const result = await oracleContract.contractViews
+      .get_price_with_timestamp(ASSET_CODE)
+      .executeView({ viewCaller: this.ovenProxyAddress })
+    const assetData = result
 
     return {
-      time: new Date(assetData.lastUpdateTime),
-      price: assetData.computedPrice, // Note: assetData.computedPrice is a BigNumber
+      time: new Date(Math.floor(new Date(assetData.last_update_timestamp).getTime() / 1000)), // taquito returns timestamp in ms not s
+      price: assetData.price, // Note: assetData.price is a BigNumber
     }
   }
 }
